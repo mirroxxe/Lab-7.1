d1dd13831a7f617742f6a4a89510f61b32ca8f02
Volodya <chuchman002@gmail.com>
2023-11-09 22:54:30 +0200

    Add project files.

---
+++ Lab 7.1/Lab 7.1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+void Create(int a[][6], const int Rows, const int ColCount, const int Low, const int High);
+void Print(int a[][6], const int Rows, const int ColCount);
+void Sort(int a[][6], const int Rows, const int ColCount);
+void Calc(int a[][6], const int Rows, const int ColCount, int& S, int& k);
+
+int main() {
+    srand((unsigned)time(NULL));
+    const int Rows = 7;
+    const int ColCount = 6;
+    int Low = 9;
+    int High = 61;
+    int a[Rows][ColCount];
+
+    Create(a, Rows, ColCount, Low, High);
+    cout << "Original Matrix:" << endl;
+    Print(a, Rows, ColCount);
+    Sort(a, Rows, ColCount);
+    cout << "Sorted Matrix:" << endl;
+    Print(a, Rows, ColCount);
+    cout << "Modified Matrix:" << endl;
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < ColCount; j++) {
+            int value = (a[i][j] % 2 == 1 || (i + j) % 7 == 0) ? 0 : 1;
+            if (a[i][j] == 0) {
+                value = 1; 
+            }
+            cout << setw(4) << value;
+        }
+        cout << endl;
+    }
+
+    int S = 0;
+    int k = 0;
+    Calc(a, Rows, ColCount, S, k);
+    cout << "S = " << S << endl;
+    cout << "k = " << k << endl;
+
+    return 0;
+}
+
+void Create(int a[][6], const int Rows, const int ColCount, const int Low, const int High) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < ColCount; j++) {
+            a[i][j] = Low + rand() % (High - Low + 1);
+        }
+    }
+}
+
+void Print(int a[][6], const int Rows, const int ColCount) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < ColCount; j++) {
+            cout << setw(4) << a[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void Sort(int a[][6], const int Rows, const int ColCount) {
+    for (int i = 0; i < Rows; i++) {
+        sort(a[i], a[i] + ColCount);
+    }
+
+    int b[6][7]{};
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < ColCount; j++) {
+            b[j][i] = a[i][j];
+        }
+    }
+
+    for (int i = 0; i < ColCount; i++) {
+        sort(b[i], b[i] + Rows);
+    }
+
+    for (int i = 0; i < ColCount; i++) {
+        for (int j = 0; j < Rows; j++) {
+            a[j][i] = b[i][j];
+        }
+    }
+}
+
+void Calc(int a[][6], const int Rows, const int ColCount, int& S, int& k) {
+    S = 0;
+    k = 0;
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < ColCount; j++) {
+            if (a[i][j] % 2 == 0 && (i + j) % 7 != 0) {
+                S += a[i][j];
+                k++;
+                a[i][j] = 0;
+            }
+        }
+    }
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 7.1/Lab 7.1.cpp"
+
+TEST_CLASS(UnitTest1)
+{
+public:
+    TEST_METHOD(TestCalc)
+    {
+        const int Rows = 7;
+        const int ColCount = 6;
+        int a[Rows][ColCount]{};
+
+        int testMatrix[Rows][ColCount] = {
+            {10, 12, 16, 25, 29, 30},
+            {10, 16, 16, 26, 38, 43},
+            {11, 17, 22, 34, 39, 43},
+            {12, 19, 24, 38, 43, 52},
+            {13, 21, 29, 38, 46, 54},
+            {13, 29, 31, 44, 50, 60},
+            {30, 37, 45, 47, 60, 61}
+        };
+
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < ColCount; j++) {
+                a[i][j] = testMatrix[i][j];
+            }
+        }
+
+        int S = 0;
+        int k = 0;
+        Calc(a, Rows, ColCount, S, k);
+
+        int expectedS = 690;
+        int expectedK = 21;
+
+        std::cout << "S = " << S << std::endl;
+        std::cout << "k = " << k << std::endl;
+
+        Assert::AreEqual(S, expectedS);
+        Assert::AreEqual(k, expectedK);
+    }
+};
